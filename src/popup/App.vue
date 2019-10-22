dd2a53cc0ee9a96f3a6b1025b5265faeaba79515
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2019-10-22 10:25:13 +0900

    First commit.

---
+++ src/popup/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Hello world!</p>
+    <p>12345</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
