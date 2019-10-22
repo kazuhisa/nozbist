063e829d36eb09c557827a8e8c7e1ef3a25b4394
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2019-10-22 10:57:34 +0900

    add milligram.css

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <p>Hello world!</p>
-    <p>12345</p>
+    <h3>Nozbe Chrome Extension</h3>
+    <a target="_blank" href="https://api.nozbe.com:3000/login?client_id=c09481e1e01e60cc585ba6631277980b6f17dcda">Login</a>
   </div>
 </template>
 
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App';
+import './milligram.min.css';
 
 global.browser = require('webextension-polyfill');
 Vue.prototype.$browser = global.browser;
