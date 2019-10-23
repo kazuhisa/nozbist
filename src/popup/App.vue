a0b280ef296cbaadea2b4198d20fb56f2aa9c2fc
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2019-10-23 19:48:16 +0900

    add login methods.

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Nozbe Chrome Extension</h3>
-    <a target="_blank" href="https://api.nozbe.com:3000/login?client_id=c09481e1e01e60cc585ba6631277980b6f17dcda">Login</a>
+    <button v-on:click="login">Login</button>
   </div>
 </template>
 
@@ -9,6 +9,19 @@
 export default {
   data() {
     return {};
+  },
+  methods: {
+    login(event) {
+      chrome.identity.launchWebAuthFlow(
+        {
+          url: 'https://api.nozbe.com:3000/login?client_id=c09481e1e01e60cc585ba6631277980b6f17dcda',
+          interactive: true,
+        },
+        responseUrl => {
+          // 後述
+        }
+      );
+    },
   },
 };
 </script>
