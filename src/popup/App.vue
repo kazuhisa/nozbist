1e4f7fe0b45fcc91a7608e1dc62e8f95a3d31e9f
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2019-11-10 19:00:08 +0900

    add entry form.

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <div v-if="access_token">
-      <p>
-        <!-- タイトルとURLを表示 -->
-        {{ title }}
-      </p>
+      <fieldset>
+        <label for="titleField">title</label>
+        <input v-model="title" type="text" id="titleField" />
+        <label for="commentField">comment</label>
+        <textarea v-model="url" id="commentField"> </textarea>
+        <button type="button" name="addRecord">
+          Adding a new task.
+        </button>
+      </fieldset>
     </div>
     <div v-else>
       <h3>Nozbe Chrome Extension</h3>
@@ -16,7 +21,10 @@
 <script>
 export default {
   data: function() {
-    return { title: '' };
+    return {
+      title: '',
+      url: '',
+    };
   },
   methods: {
     login(event) {
@@ -36,6 +44,7 @@ export default {
     },
     set_title(tab) {
       this.title = tab[0].title;
+      this.url = tab[0].url;
     },
   },
   created() {
